d6a55252a64ba2752a681eedd5fc43ea11387ee0
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-09-03 15:31:17 +0300

    Option Success Status

--- middlewares/10-cors.js
+++ middlewares/10-cors.js
@@ -2,7 +2,8 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: true,
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200
 };
 
 module.exports = cors(corsOptions);
