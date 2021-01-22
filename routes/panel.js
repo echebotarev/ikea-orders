3489539cfe16fe0a5d6b3be2b86e21bb15cad42e
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-01-22 16:23:56 +0300

    Получение заказов по времени 5

--- routes/panel.js
+++ routes/panel.js
@@ -9,13 +9,10 @@ router.get('/SNMb0pmYka0bSi/:date?', async (req, res) => {
   const { date } = req.params;
   const payload = Object.assign(
     { checkout: true },
-    date && isValidDate(date)
+    date && isValidDate(new Date(date))
       ? { checkoutCreatedAt: { $gte: new Date(date) } }
       : {}
   );
-
-  console.log('Date', date);
-  console.log('Payload', payload);
 
   const orders = await db.Order.getOrders(payload);
   res.send(orders);
